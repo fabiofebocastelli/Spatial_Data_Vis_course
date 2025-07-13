4d50958f91254cf3661e460034959484ce744a85
Fabio Castelli <94614136+fabiofebocastelli@users.noreply.github.com>
2025-07-13 12:59:40 +0200

    Update Day2_02_ridgeline.R

--- Code/Day2_02_ridgeline.R
+++ Code/Day2_02_ridgeline.R
@@ -1,220 +1,58 @@
-# R code for classifying images
-
-# install.packages("patchwork")
+# The following code is related to ridgeline plots in R
 
 library(terra)
 library(imageRy)
 library(ggplot2)
 library(patchwork)
-
-im.list()
-
-mato1992 = im.import("matogrosso_l5_1992219_lrg.jpg")
-mato1992 = flip(mato1992)
-plot(mato1992)
-
-mato2006 = im.import("matogrosso_ast_2006209_lrg.jpg")
-mato2006 = flip(mato2006)
-plot(mato2006)
-
-mato1992c = im.classify(mato1992, num_clusters=2)
-# class 1 = human
-# class 2 = forest
-
-mato2006c = im.classify(mato2006, num_clusters=2)
-# class 1 = forest
-# class 2 = human
-
-f1992 = freq(mato1992c)
-tot1992 = ncell(mato1992c)
-prop1992 = f1992 / tot1992
-perc1992 = prop1992 * 100
-
-# human = 17%, forest = 83%
-
-perc2006 = freq(mato2006c) * 100 / ncell(mato2006c)
-
-# human = 54%, forest = 45%
-
-# Creating dataframe
-
-class = c("Forest","Human")
-y1992 = c(83,17)
-y2006 = c(45,55)
-tabout = data.frame(class, y1992, y2006)
-
-p1 = ggplot(tabout, aes(x=class, y=y1992, color=class)) + 
-  geom_bar(stat="identity", fill="white") + 
-  ylim(c(0,100))
-
-p2 = ggplot(tabout, aes(x=class, y=y2006, color=class)) + 
-  geom_bar(stat="identity", fill="white") + 
-  ylim(c(0,100))
-
-p1 + p2
-p1 / p2
-
-p0 = im.ggplot(mato1992)
-p00 = im.ggplot(mato2006)
-  
-p0 + p00 + p1 + p2
-
-% https://stackoverflow.com/questions/52684424/show-the-percentage-instead-of-count-in-histogram-using-ggplot2-r
-
-
-## alternative script
-
-# how to deal with land use in the tidyverse
-
-library(terra)
-library(imageRy)
-library(ggplot2)
-library(patchwork)
+library(viridis)
 library(RStoolbox)
 
-im.list()
+# for those who do not have imageRy
+install.packages("ggridges")
+library(ggridges)
 
-m1992 <- im.import("matogrosso_l5_1992219_lrg.jpg")
+ndvi <- im.import("Sentinel2_NDVI_")
 
-m1992 <- rast("~/Desktop/matogrosso_l5_1992219_lrg.jpg")
-plot(m1992)
+ndvi02 <- rast("~/Downloads/Sentinel2_NDVI_2020-02-21.tif")
+ndvi05 <- rast("~/Downloads/Sentinel2_NDVI_2020-05-21.tif")
+ndvi08 <- rast("~/Downloads/Sentinel2_NDVI_2020-08-01.tif")
+ndvi11 <- rast("~/Downloads/Sentinel2_NDVI_2020-11-27.tif")
+ndvi <- c(ndvi02, ndvi05, ndvi08, ndvi11)
 
-set.seed(42)
-m1992c <- im.classify(m1992, num_clusters=2)
+# vegetation is reflecting NIR
+# vegetation is absorbing red
+# 8 bit = 256 values (0-255)
+# NIR = 255
+# red = 0 
+# DVI = NIR - red = 255 - 0 = 255
+# NDVI = (NIR - red) / (NIR + red)
+# NDVI = (255 - 0) / (255 + 0) = 1 / 1 = 1
 
-# class 1 = forest
-# class 2 = human
+# 4 bit = 2^4 = 16 values (0-15)
+# NIR = 15
+# red = 0
+# DVI = NIR - red = 15- 0 = 15
+# NDVI = (NIR - red) / (NIR + red)
+# NDVI = (15 - 0) / (15 + 0) = 1
 
-m2006 <- im.import("matogrosso_ast_2006209_lrg.jpg")
-m2006 <- rast("~/Desktop/matogrosso_ast_2006209_lrg.jpg")
-plot(m2006)
+im.ridgeline(ndvi, scale=1)
+names(ndvi) = c("m02_feb", "m05_may", "m08_aug", "m11_nov")
 
-set.seed(42)
-m2006c <- im.classify(m2006, num_clusters=2)
-
-# class 1 = human
-# class 2 = forest
-
-##### The following code will work also in case you have land use maps
-# As an example: 
-# freq(map1)
-
-#  frequencies
-f1992 <- freq(m1992c)
-p1992 <- f1992 * 100 / ncell(m1992c)
-
-# forest = 83%
-# human = 17%
-
-f2006 <- freq(m2006c)
-p2006 <- f2006 * 100 / ncell(m2006c)
-
-# human = 55%
-# forest = 45%
-
-class = c("Forest","Human")
-y1992 = c(83, 17)
-y2006 = c(45, 55)
-tabout = data.frame(class, y1992, y2006)
-
-p1 <- ggRGB(m1992, 1, 2, 3, stretch="lin") + ggtitle("Landsat image of 1992")
-
-p2 <- ggplot(tabout, aes(x=class, y=y1992, color=class)) +
-  geom_bar(stat="identity", fill="white") + ggtitle("Land use of 1992") + ylim(c(0,100))
-
-p1 + p2
-
-p3 <- ggRGB(m2006, 1, 2, 3, stretch="lin") + ggtitle("Aster image of 2006")
-
-p4 <- ggplot(tabout, aes(x=class, y=y2006, color=class)) +
-  geom_bar(stat="identity", fill="white") + ggtitle("Land use of 2006") + ylim(c(0,100))
-
-p3 + p4
-
-p2 + p4
-
-(p1 | p3) / (p2 | p4)
-
-pdf("~/Desktop/output_land_change.pdf")
-(p1 | p3) / (p2 | p4)
+pdf("~/Desktop/outridge.pdf")
+im.ridgeline(ndvi, scale=2, palette="mako")
 dev.off()
 
+# probability of agriculture 1992 = continuous maps
+# probability of agriculture 2006 = continuous maps
 
-#----- Day 2
 
-# Coloring bars
 
-ggplot(tabout, aes(x=class, y=y2006, fill=class, color=class)) + 
-  geom_bar(stat="identity") + 
-  ylim(c(0,100))
 
-# https://www.sthda.com/english/wiki/ggplot2-barplots-quick-start-guide-r-software-and-data-visualization
 
-# Horizontal bars
 
-p1 = ggplot(tabout, aes(x=class, y=y1992, color=class)) +
-  geom_bar(stat="identity", fill="white") +
-  ylim(c(0,100)) +
-  coord_flip()
 
-p2 = ggplot(tabout, aes(x=class, y=y2006, color=class)) +
-  geom_bar(stat="identity", fill="white") +
-  ylim(c(0,100)) +
-  coord_flip()
 
-p1 / p2
 
-classall = c("Forest1992","Human1992","Forest2006","Human2006")
-percentage = c(83,17,45,55)
-all = data.frame(classall, percentage)
-
-# Stacked barplot with multiple groups
-ggplot(data=all, aes(x=classall, y=percentage, color=classall)) +
-  geom_bar(stat="identity", fill="white")
-
-#---- Solar Orbiter
-
-im.list()
-
-solar = im.import("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
-
-# Exercise: classify the image in three classes - im.classify()
-solarc = im.classify(solar, num_clusters=3)
-
-# Plot the original image beside the classified one
-dev.off()
-im.multiframe(1,2)
-plot(solar)
-plot(solarc)
-
-# 3 = low
-# 1 = medium
-# 2 = high
-
-dev.off()
-solarcs = subst(solarc, c(3,1,2), c("c1_low","c2_medium","c3_high"))
-plot(solarcs)
-
-# Exercise: calculate the percentages of the Sun enrgy classes with one line of code
-
-percsolar = freq(solarcs)$count * 100 / ncell(solarcs)
-
-# [1] 37.92693 41.04342 21.02965
-# [1] 38 41 21
-
-# create dataframe
-class = c("c1_low","c2_medium","c3_high")
-# check names in the object
-perc = c(38,41,21)
-tabsol = data.frame(class, perc)
-
-# final ggplot
-
-ggplot(tabsol, aes(x=class, y=perc, fill=class, color=class)) +
-  geom_bar(stat="identity") +
-#  ylim(c(0,100)) 
-  coord_flip() 
-# + scale_y_reverse()
 
 
 
